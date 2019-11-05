fc7858147c10aa33a97c6226b609a44d43fcb221
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-05 01:34:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,12 +17,12 @@ require "pry"
 phrase = "I LOVE YOU GRANDMA!"
 
 def speak_to_grandma(phrase)
-
+binding.pry
 if phrase == "I LOVE YOU GRANDMA!"
 return "I LOVE YOU TOO PUMPKIN!"
 elsif  phrase.downcase == phrase
 return "HUH?! SPEAK UP, SONNY!"
-binding.pry
+
 else  phrase.upcase == phrase
 return "NO, NOT SINCE 1938!"
 
